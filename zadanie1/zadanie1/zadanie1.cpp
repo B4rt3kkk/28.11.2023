ecb32fc56a35bc9f2fadd5e2b5685f909c9b8d4e
FilipZembrzuski <fzembrzuski@gmail.com>
2023-11-28 10:32:28 +0100

    eloelemakrelo

---
+++ zadanie1/zadanie1/zadanie1.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "");
+    int* tablica;
+    int ile = 5;
+    tablica = new int[ile];
+
+    cout << "Dynamiczne Adresy do przydzielania 5 ele = " << (int)tablica << endl;
+    cout << "Adres własny wskaznika tablica = " << (int)&tablica << endl;
+
+    cout << (int)&tablica[0] << endl;
+    cout << (int)&tablica[1] << endl;
+    cout << (int)&tablica[2] << endl;
+    cout << (int)&tablica[3] << endl;
+    cout << (int)&tablica[4] << endl;
+
+    // inny sposób wyświetlania adresów z tablicy 
+    cout << endl << endl;
+    for (int i = 0; i < ile; i++)
+    {
+        cout << (int)tablica << endl;
+        tablica++;
+    }
+    //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+    return 0;
+}
---
+++ zadanie2/zadanie2/zadanie2.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <time.h>
+using namespace std;
+
+int main()
+{
+	srand(time(NULL));
+	clock_t start, stop;
+
+
+	start = clock();
+
+	cout << "Dynamiczne przydzielanie pamieci dla tablicy - WSKAZNIK" << endl;
+	int rozmiarTablicy;
+	int los;
+	cout << "Podaj romzmiar tablicy = "; cin >> rozmiarTablicy;
+	int* tablica;
+	tablica = new int[rozmiarTablicy];
+
+	for (int i = 0; i < rozmiarTablicy; i++) 
+	{
+		los = rand() % 100 + 1;
+		tablica[i] = los;
+	}
+	for (int i = 0; i < rozmiarTablicy; i++) 
+	{
+		cout << "Index nr " << i << " = " << tablica[i] << "Adres komurki: " << (int)&tablica[i] << endl;
+	}
+
+	stop = clock();
+
+	double czasWyswietlaniaAplikacji = (double)(stop - start) / CLOCKS_PER_SEC;
+
+	cout << "czas wyswietlania: " << czasWyswietlaniaAplikacji << endl;
+
+	delete[] tablica;	
+
+	return EXIT_SUCCESS;
+}
+
